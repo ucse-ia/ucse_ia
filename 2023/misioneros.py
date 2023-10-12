7eed19c7801b22c6be2bf25f50d5bc79fe0929d1
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-10-12 19:16:45 -0300

    Misioneros y canibales 2023

--- 2023/misioneros.py
+++ 2023/misioneros.py
@@ -16,23 +16,63 @@ from simpleai.search.viewers import BaseViewer, WebViewer
 INITIAL = ((), ("M", "M", "M", "C", "C", "C"), 1)
 
 
+def orilla_ok(gente):
+    cantidad_misioneros = gente.count("M")
+    cantidad_canibales = gente.count("C")
+
+    return (cantidad_misioneros >= cantidad_canibales) or not cantidad_misioneros
+
+
 class MisionerosProblen(SearchProblem):
     def actions(self, state):
+        izq, der, canoa = state
         available_actions = []
+
+        origen = state[canoa]
+
+        posibilidades_tripulantes = []
+        posibilidades_tripulantes.extend([(p, ) for p in origen])
+        posibilidades_tripulantes.extend(combinations(origen, 2))
+        posibilidades_tripulantes = set(posibilidades_tripulantes)
+
+        for tripulantes in posibilidades_tripulantes:
+            estado_resultante = self.result(state, tripulantes)
+            if orilla_ok(estado_resultante[0]) and orilla_ok(estado_resultante[1]):
+                available_actions.append(tripulantes)
 
         return available_actions
 
     def result(self, state, action):
-        ...
+        izq, der, canoa = state
+
+        origen = list(state[canoa])
+        destino = list(state[1 - canoa])
+
+        for persona in action:
+            origen.remove(persona)
+            destino.append(persona)
+
+        canoa = 1 - canoa
+
+        origen = tuple(origen)
+        destino = tuple(destino)
+
+        if canoa == 0:
+            return destino, origen, canoa
+        else:
+            return origen, destino, canoa
+
 
     def cost(self, state, action, state2):
         return 1
 
     def is_goal(self, state):
-        ...
+        izq, der, canoa = state
+        return not der
 
     def heuristic(self, state):
-        ...
+        izq, der, canoa = state
+        return len(der) - 1
 
 
 
